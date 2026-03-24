5d5d22449b52b7c0ea5df795500b6735ff0ddac3
MaxTidwell <mtidwell@byupathway.edu>
2026-03-24 16:53:52 -0600

    week4 Learning activities and reorganizing of files

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -114,7 +114,7 @@ function renderCourses(list) {
         card.innerHTML = `
             <h3>${course.completed ? "&check; " : ""} ${course.subject} ${course.number}: ${course.title}</h3>
         `;
-
+        card.addEventListener('click', () => displayCourseDetails(course));
         courseContainer.appendChild(card);
     });
 
@@ -140,3 +140,23 @@ cseBtn.addEventListener('click', () => {
 
 
 renderCourses(courses);
+
+// dialog popup
+const courseDetails = document.querySelector('#course-details');
+
+function displayCourseDetails(course) {
+    courseDetails.innerHTML = `
+        <button id="closeModal">X</button>
+        <h2>${course.subject} ${course.number}</h2>
+        <h3>${course.title}</h3>
+        <p><strong>Credits</strong>: ${course.credits}</p>
+        <p><strong>Certificate</strong>: ${course.certificate}</p>
+        <p>${course.description}</p>
+        <p><strong>Technologies</strong>: ${course.technology.join(', ')}</p>
+    `;
+    courseDetails.showModal();
+
+    document.querySelector('#closeModal').addEventListener('click', () => {
+        courseDetails.close();
+    });
+}
